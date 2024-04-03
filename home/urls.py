71ff25258e622850ec7d86e6a6f3cbdb17428bde
rouhi <r.shafahi11@gmail.com>
2024-04-03 05:53:11 +0330

    add Post model into admin pannel and run the migrate and makemigrations and createsuperuser

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'home'
-url_patterns = [
+urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
