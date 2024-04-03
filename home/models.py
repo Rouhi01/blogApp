11cfd9bde03c2b65d105f7096461d0db192b56c8
rouhi <r.shafahi11@gmail.com>
2024-04-03 05:13:25 +0330

    create Post model and media setting

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
